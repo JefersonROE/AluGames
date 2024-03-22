064afcded9fef83858401008ab0a02a089d5dbc4
JefersonROE <jefersonrodriguesoe@gmail.com>
2024-03-22 13:07:15 -0300

    Initial commit

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,12 @@
+package org.example
+
+class Jogo {
+    var titulo = ""
+    var capa = ""
+    val descricao = ""
+    override fun toString(): String {
+        return "Jogo(titulo='$titulo', capa='$capa', descricao='$descricao')"
+    }
+
+
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+package org.example
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+    val response = client
+        .send(request, BodyHandlers.ofString())
+    val json = response.body()
+    println(json)
+
+    val meuJogo = Jogo ()
+    meuJogo.titulo = "Batman: Arkham Asylam Game of the Year Edition"
+    meuJogo.capa = "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1681938587"
+
+    println(meuJogo)
+
+}
